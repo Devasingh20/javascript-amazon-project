82989b14497bf431e8941705cada9804b9f33eb3
Deva Singh <deva81002@gmail.com>
2025-07-02 20:45:51 +0530

    change the css

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -14,7 +14,7 @@ products.forEach((product) => {
 
         <div class="product-rating-container">
           <img class="product-rating-stars" src="images/ratings/rating-${(product.rating.stars) * 10}.png">
-          <div class="product-rating-count link-primary">
+          <div class="product-rating-count link-primary1">
             ${product.rating.count} ratings
           </div>
         </div>
