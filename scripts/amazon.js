20ce35e94f2973feb6cacaf5807302ca34a41663
Deva Singh <deva81002@gmail.com>
2025-08-05 23:53:42 +0530

    // Extra feature: searching by pressing "Enter" on the keyboard.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -233,5 +233,14 @@ function renderFilteredProducts(filteredProducts) {
     const search = document.querySelector('.js-search-bar').value
     window.location.href = `amazon.html?search=${search}`;
   });
+
+document.querySelector('.js-search-bar')
+    .addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        const searchTerm = document.querySelector('.js-search-bar').value;
+        window.location.href = `amazon.html?search=${searchTerm}`;
+      }
+    });
+
   return renderFilteredProducts;
 }
